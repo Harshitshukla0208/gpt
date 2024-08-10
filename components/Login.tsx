2393a224e265e5c398c7e293c37060619349de4e
Harshit Shukla <mrharshit2409@gmail.com>
2024-08-10 12:12:56 +0530

    login part completed

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,9 +1,25 @@
+"use client"
+import Image from 'next/image'
 import React from 'react'
+import logo from '../public/gpt-logo-light.svg'
+import { signIn } from 'next-auth/react'
 
 const Login = () => {
     return (
-        <div>
-            login page
+        <div className='h-screen flex flex-col items-center justify-center text-center'>
+            <Image
+                src={logo}
+                width={150}
+                height={150}
+                alt="logo"
+                color='white'
+            />
+            <button 
+                onClick={() => signIn('google')} 
+                className='text-white font-bold text-3xl animate-pulse'
+            >
+                Sign In to use GPT
+            </button>
         </div>
     )
 }
